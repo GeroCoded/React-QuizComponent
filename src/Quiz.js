296465c6484dc45c257aa5e32d685c1b03b2041c
Luis Fernando Gerónimo Carranza <luis.geronimo@gnp.com.mx>
2021-02-28 23:13:30 -0600

    Completed module

--- src/Quiz.js
+++ src/Quiz.js
@@ -21,6 +21,10 @@ class Quiz extends Component {
         });
     }
 
+    handleResetClick() {
+        this.setState({ quiz_position: 1 });
+    }
+
     render() {
 
         const isQuizEnd = this.state.quiz_position - 1 === quizData.quiz_questions.length;
@@ -28,7 +32,7 @@ class Quiz extends Component {
         return (
             <div>
                 { isQuizEnd 
-                    ? <QuizEnd />
+                    ? <QuizEnd resetClickHandler={this.handleResetClick.bind(this)} />
                     : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} showNextQuestionHandler={this.showNextQuestion.bind(this)} />
                 }
                 {/* <div className="QuizQuestion">
